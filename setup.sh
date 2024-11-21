b00e8e8dcef61e6745c0ef1b775f7ed01d874f4f
Yosuf Ali <yosufali@users.noreply.github.com>
2024-11-21 23:49:27 +0000

    Symlink .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,9 +1,22 @@
+echo "== git setup =="
+GITCONFIG_FILE=~/.gitconfig
+if [ -f "$GITCONFIG_FILE" ]; then
+    echo "removing existing $GITCONFIG_FILE."
+    rm $GITCONFIG_FILE
+fi
+
+ln -s dev/dotfiles/git/.gitconfig ~/.gitconfig
+echo "created symlink to new .gitconfig"
+
+echo "== zsh setup =="
 # set .zshrc
 ZSHRC_FILE=~/.zshrc
 if [ -f "$ZSHRC_FILE" ]; then
     echo "removing existing $ZSHRC_FILE."
     rm $ZSHRC_FILE
 fi
-# first argument is understood as a relative path to the parent directory of the symlink
 ln -s dev/dotfiles/zsh/.zshrc ~/.zshrc
 echo "created symlink to new .zshrc"
+
+# note:
+# first ln argument is understood as a relative path to the parent directory of the symlink
